9a8c908bc91e4a0256128eb882dde1db4df126d7
niuhaijun <nhjemail@163.com>
2018-11-17 00:25:22 +0800

    素数收集器

--- src/main/java/java8/collector/CollectorTest.java
+++ src/main/java/java8/collector/CollectorTest.java
@@ -1,7 +1,9 @@
 package java8.collector;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import org.junit.Test;
 
@@ -11,11 +13,40 @@ import org.junit.Test;
  */
 public class CollectorTest {
 
+  /**
+   * 使用自定义的收集器统计素数
+   */
   @Test
-  public void test() {
+  public void generatePrimeTest() {
 
     Map<Boolean, List<Integer>> map = IntStream.rangeClosed(2, 10).boxed()
         .collect(new PrimeNumbersCollector());
     map.get(Boolean.TRUE).stream().forEach(System.out::println);
   }
+
+  /**
+   * 使用预定义的收集器统计素数
+   */
+  @Test
+  public void getPrimeTest() {
+
+    Map<Boolean, List<Integer>> map = IntStream.rangeClosed(2, 100).boxed().collect(
+        Collectors.partitioningBy(element -> isPrime(element)));
+
+    System.out.println(map.get(Boolean.TRUE));
+  }
+
+
+  /**
+   * 判断一个数是否为素数
+   *
+   * @param n 被测试的数
+   * @return 如果为素数返回true, 否则返回false。
+   */
+  private boolean isPrime(int n) {
+
+    int root = (int) Math.sqrt(n);
+    return IntStream.rangeClosed(2, root).noneMatch(element -> n % element == 0);
+  }
+
 }
