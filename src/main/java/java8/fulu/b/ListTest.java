562424f8cf1548ed684bdec58f0eeb805c016da2
niuhaijun <1973202088@qq.com>
2018-11-24 23:12:25 +0800

    类库更新

---
+++ src/main/java/java8/fulu/b/ListTest.java
@@ -0,0 +1,71 @@
+package java8.fulu.b;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import org.junit.Test;
+
+/**
+ * List
+ *    replaceIf
+ *    sort
+ *
+ * Collection
+ *    removeIf
+ *
+ *
+ * @author niuhaijun
+ * @date 2018/11/24 22:09
+ */
+public class ListTest {
+
+  @Test
+  public void testSort() {
+
+    List<Integer> list = null;
+
+    list = getList();
+    System.out.println(list);
+
+    list.sort(Comparator.naturalOrder());
+    System.out.println(list);
+  }
+
+  @Test
+  public void testReplaceAll() {
+
+    List<Integer> list = getList();
+
+    System.out.println(list);
+    list.replaceAll(t -> t * t);
+    System.out.println(list);
+
+  }
+
+  @Test
+  public void testRemoveIf() {
+
+    List<Integer> list = getList();
+    list.removeIf(t -> t <= 1);
+
+    System.out.println(list);
+  }
+
+  @Test
+  public void testForEach() {
+
+    List<Integer> list = getList();
+    list.forEach(System.out::println);
+  }
+
+  private List<Integer> getList() {
+
+    return new ArrayList<Integer>() {
+      {
+        add(3);
+        add(2);
+        add(1);
+      }
+    };
+  }
+}
