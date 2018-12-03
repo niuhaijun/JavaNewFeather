4512e55bb78006313de1f2350f5d48e47858f5f3
niuhaijun <1973202088@qq.com>
2018-12-03 15:45:57 +0800

    测试StringBuilder类

--- src/main/java/niu/App.java
+++ src/main/java/niu/App.java
@@ -1,5 +1,7 @@
 package niu;
 
+import org.junit.Test;
+
 /**
  * Hello world!
  *
@@ -7,6 +9,29 @@ package niu;
 public class App {
 
   public static void main(String[] args) {
+
     System.out.println("Hello World!");
+  }
+
+  @Test
+  public void testAppend() {
+
+    StringBuilder builder = new StringBuilder();
+
+    builder.append(1);
+    System.out.println(builder);
+
+    builder.append(1);
+    System.out.println(builder);
+
+    builder.append(1);
+    System.out.println(builder);
+
+    String message = "abc%sabc";
+    if (3 > 2) {
+      message = String.format(message, "1111");
+    }
+    System.out.println(message);
+
   }
 }
