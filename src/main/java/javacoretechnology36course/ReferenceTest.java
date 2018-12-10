610a6e66872588fba4441011afea50bde7be7689
niuhaijun <niuhaijun@100tal.com>
2018-12-10 14:18:06 +0800

    虚引用

---
+++ src/main/java/javacoretechnology36course/ReferenceTest.java
@@ -0,0 +1,42 @@
+package javacoretechnology36course;
+
+import java.lang.ref.PhantomReference;
+import java.lang.ref.Reference;
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.SoftReference;
+
+/**
+ * @author niuhaijun
+ * @date 2018/12/10 11:06
+ */
+public class ReferenceTest {
+
+  public static void main(String[] args) {
+
+    Object counter = new Object();
+    System.out.println(counter);
+
+    ReferenceQueue refQueue = new ReferenceQueue();
+    Reference<Object> p = new PhantomReference<>(counter, refQueue);
+
+    counter = null;
+    System.gc();
+
+    try {
+      /**
+       * remove是一个阻塞方法，可以指定timeout，或者选择一直阻塞
+       */
+      Reference<Object> ref = refQueue.remove(5_000L);
+      if (ref != null) {
+        // do something
+        System.out.println("counter已被GC");
+      }
+    } catch (InterruptedException e) {
+      // handle it
+      e.printStackTrace();
+    }
+  }
+
+  ThreadLocal<Integer> threadLocal = new ThreadLocal<>();
+
+}
